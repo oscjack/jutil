53e1d2eb0fe5644a5468519eb8df21cadd02ac40
jack yin <waring3yin@icloud.com>
2016-09-07 13:55:44 +0800

    update array equal

--- src/Jutil/Arr.php
+++ src/Jutil/Arr.php
@@ -4,8 +4,12 @@ namespace Jutil;
 
 class Arr
 {
-	public static function equal($strict = false)
+	public static function equal($a = [], $b = [], $strict = false)
 	{
+		if (count($a) !== count($b)) {
+			return false;
+		}
+		
 		$x = array_values($a);
 		$y = array_values($b);
 
